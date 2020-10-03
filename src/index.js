54402c76c6a33c5ec1c500d4da3e6d5e8b69fcf5
Amir Sekhavati <amir.sekhavati@gmail.com>
2020-10-03 17:21:23 +0100

    - add sequence function

--- src/index.js
+++ src/index.js
@@ -129,9 +129,10 @@ const from = (collection) => ({
   dropConsecutiveRepeats: () => R.dropRepeats(),
 });
 
+// @todo: add multi-transformation function, eg: tranform(obj).byTransforming(keys).with(transformers)
 const transform = (obj) => ({
-  byApplying: (predicate) => ({
-    to: (key) => R.evolve({ [key]: predicate }, obj),
+  byApplying: (transformer) => ({
+    to: (key) => R.evolve({ [key]: transformer }, obj),
   }),
 });
 
@@ -156,6 +157,21 @@ const copy = (obj) => ({
     defaultToUndefined ? R.pickAll(keys, obj) : R.pick(keys, obj),
 });
 
+// seedValue: 10;
+// sequenceTerminator: (n) => n < 50;
+// sequenceRule: (n) => n + 10;
+// sequenceTermMapFn: n => n (by default) ???
+// @todo: potentially introduce sequenceTermMapFn param, ie: [n, ...] becomes [mapFn(n), ...]
+const sequence = (sequenceRule) => ({
+  startingWith: (seed) => ({
+    untilCondition: (sequenceTerminator) => {
+      const f = (n) => (sequenceTerminator(n) ? false : [n, sequenceRule(n)]);
+      return R.unfold(f, seed);
+    },
+    untilLength: (length) => null, // @todo: create a sequence with max length entries
+  }),
+});
+
 const rpp = {
   adjust,
   split,
@@ -172,6 +188,7 @@ const rpp = {
   transform,
   merge,
   copy,
+  sequence,
 };
 
 module.exports = rpp;
