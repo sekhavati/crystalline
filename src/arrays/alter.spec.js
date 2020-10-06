e2b06769af1bf596e677c322bd624a228e5b316c
Amir Sekhavati <amir.sekhavati@gmail.com>
2020-10-06 08:46:52 +0100

    add unit tests for array.alter.* functions

---
+++ src/arrays/alter.spec.js
@@ -0,0 +1,85 @@
+const R = require("ramda");
+const { alter } = require("./alter");
+
+describe("alter", () => {
+  describe("byApplyingFn", () => {
+    describe("atIndex", () => {
+      it("should return a new array with the function supplied at the given index", () => {
+        const input = ["a", "b", "c", "d"];
+
+        const result = alter(input).byApplyingFn(R.toUpper).atIndex(1);
+
+        expect(input).not.toEqual(result);
+        expect(result).toEqual(["a", "B", "c", "d"]);
+      });
+    });
+  });
+
+  describe("byInsertingBetweenEachItem", () => {
+    it("should return a new array with the value supplied inserted between each item", () => {
+      const input = ["b", "n", "n", "s"];
+
+      const result = alter(input).byInsertingBetweenEachItem("a");
+
+      expect(input).not.toEqual(result);
+      expect(result).toEqual(["b", "a", "n", "a", "n", "a", "s"]);
+    });
+  });
+
+  describe("byMovingItemAtIndex", () => {
+    describe("to", () => {
+      it("should return a new array with the item at the index specified moved to the chosen index", () => {
+        const input = ["a", "b", "c", "d", "e", "f"];
+
+        const result = alter(input).byMovingItemAtIndex(0).toIndex(2);
+
+        expect(input).not.toEqual(result);
+        expect(result).toEqual(["b", "c", "a", "d", "e", "f"]);
+      });
+    });
+  });
+
+  describe("byRemovingItemsBetweenIndex", () => {
+    describe("andIndex", () => {
+      it("should return a new array with all items between the two indexes removed ", () => {
+        const input = [1, 2, 3, 4, 5, 6, 7, 8];
+
+        const result = alter(input).byRemovingItemsBetweenIndex(2).andIndex(3);
+
+        expect(input).not.toEqual(result);
+        expect(result).toEqual([1, 2, 6, 7, 8]);
+      });
+    });
+  });
+
+  describe("byRemovingDuplicates", () => {
+    it("should return a new array with any duplicates from the original removed", () => {
+      const input1 = [1, 1, 2, 1];
+      const input2 = [1, "1"];
+      const input3 = [[42], [42]];
+
+      const result1 = alter(input1).byRemovingDuplicates();
+      const result2 = alter(input2).byRemovingDuplicates();
+      const result3 = alter(input3).byRemovingDuplicates();
+
+      expect(input1).not.toEqual(result1);
+      expect(result1).toEqual([1, 2]);
+
+      expect(result2).toEqual([1, "1"]);
+
+      expect(input3).not.toEqual(result3);
+      expect(result3).toEqual([[42]]);
+    });
+  });
+
+  describe("byRemovingItems", () => {
+    it("should return a new array with any items matching those supplied removed", () => {
+      const input = [1, 2, 1, 3, 4];
+
+      const result = alter(input).byRemovingItems([1, 2]);
+
+      expect(input).not.toEqual(result);
+      expect(result).toEqual([3, 4]);
+    });
+  });
+});
