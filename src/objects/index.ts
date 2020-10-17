b6044bfd9ec34b15ede599ce1e841e6b70a691fd
Amir Sekhavati <amir.sekhavati@gmail.com>
2020-10-17 16:41:07 +0100

    delete object.within.* functions

--- src/objects/index.ts
+++ src/objects/index.ts
@@ -1,6 +1,5 @@
 import { alter } from "./alter";
 import { copy } from "./copy";
 import { merge } from "./merge";
-import { within } from "./within";
 
-export default { alter, copy, merge, within };
+export default { alter, copy, merge };
