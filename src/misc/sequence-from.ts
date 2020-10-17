facbd181550b8de819fba649852830ed6ad4fd35
Amir Sekhavati <amir.sekhavati@gmail.com>
2020-10-17 19:40:18 +0100

    remove ts-ignore

--- src/misc/sequence-from.ts
+++ src/misc/sequence-from.ts
@@ -3,8 +3,9 @@ import unfold from "ramda/src/unfold";
 const sequenceFrom = <T>(rule: (a: T) => any) => ({
   startingWith: (seed: T) => ({
     untilCondition: (terminator: (a: T) => boolean) => {
-      const f = (n: T) => (terminator(n) ? false : [n, rule(n)]);
-      // @ts-ignore
+      const f = (n: T): false | [T, T] =>
+        terminator(n) ? false : [n, rule(n)];
+
       return unfold(f, seed);
     },
   }),
