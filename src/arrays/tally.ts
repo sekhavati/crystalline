016e6780a0cc3983b2de03efbd9214efa71769a0
Amir Sekhavati <amir.sekhavati@gmail.com>
2020-10-29 21:20:45 +0000

    - add readme section for arrays.tally.*

--- src/arrays/tally.ts
+++ src/arrays/tally.ts
@@ -1,7 +1,7 @@
 import countBy from "ramda/src/countBy";
 
 const tally = <T>(arr: T[]) => ({
-  by: (predicate: (a: T) => any) => countBy(predicate)(arr),
+  by: (fn: (a: T) => any) => countBy(fn)(arr),
 });
 
 export { tally };
