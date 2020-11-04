e75f345d44c6f057d361564427b75a8f264e66d5
Amir Sekhavati <amir@sekhaconvalley.com>
2020-11-04 17:47:44 +0000

    chore: remove dry-run flags from new release script

--- release.sh
+++ release.sh
@@ -4,8 +4,8 @@ set -o pipefail
 
 yarn clean
 yarn build
-npx np --no-publish --preview
+npx np --no-publish
 cp package.json LICENSE README.md dist/
 npm login
-npm publish dist --dry-run
+npm publish dist
 npm logout
