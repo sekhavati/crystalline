be743ba5388ac5d3e6f48e1e232c9e165d7ac04e
Amir Sekhavati <amir@sekhaconvalley.com>
2020-11-10 20:53:44 +0000

    chore: remove redundant describe block

--- src/numbers/clamp.spec.ts
+++ src/numbers/clamp.spec.ts
@@ -2,12 +2,10 @@ import { clamp } from "./clamp";
 
 describe("clamp", () => {
   describe("between", () => {
-    describe("and", () => {
-      it("should restrict a number to be within the range specified", () => {
-        expect(clamp(-5).between(1, 10)).toBe(1);
-        expect(clamp(15).between(1, 10)).toBe(10);
-        expect(clamp(4).between(1, 10)).toBe(4);
-      });
+    it("should restrict a number to be within the range specified", () => {
+      expect(clamp(-5).between(1, 10)).toBe(1);
+      expect(clamp(15).between(1, 10)).toBe(10);
+      expect(clamp(4).between(1, 10)).toBe(4);
     });
   });
 });
